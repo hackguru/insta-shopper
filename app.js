df0443402db2226f5aef5f2fe7b37a4a93b85d31
Edward Rezaimehr <erezaimehr@SREZAIMEHR-MBP.home>
2015-01-31 14:49:45 -0800

    adding insta update route

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', instaSetup, db, users);
-app.use('/insta', instaSetup, db, users);
+app.use('/insta', instaSetup, db, insta);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
