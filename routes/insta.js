b3a37b695a7355e0f183be3709de8d294cb643b7
Edward Rezaimehr <erezaimehr@SREZAIMEHR-MBP.local>
2015-01-31 19:41:49 -0800

    getting media object after live feed calls

--- routes/insta.js
+++ routes/insta.js
@@ -9,13 +9,25 @@ router.get('/post', function(req, res, next) {
 
 router.post('/post', function(req, res, next) {
    // [ { changed_aspect: 'media',
-   //     object: 'user',
-   //     object_id: '1686295026',
-   //     time: 1422755996,
-   //     subscription_id: 16642046,
-   //     data: [Object] } ],
+	  // object: 'user',
+	  // object_id: '1686295026',
+	  // time: 1422761294,
+	  // subscription_id: 16642046,
+	  // data: { media_id: '910510192435687045_1686295026' } }],
 	req.body.forEach(function(currentValue){
-		console.log(currentValue);
+		var userId = currentValue.object_id;
+		var mediaId = currentValue.data.media_id;
+		reg.db.User.findOne({ instaId: userId }, function (err, user) {
+			if(!err){
+				req.instagram.use({ access_token: user.token });
+				req.instagram.media(mediaId, function(err, media, remaining, limit) {
+					console.log(media);
+				});
+			}else{
+				console.log(err);
+				//TODO : handle error
+			}	
+		});
 	});
 	res.send('respond with a resource');
 });
