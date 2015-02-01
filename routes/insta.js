3b8f9bbc381c06c083dd7533b9c8764902c43ad6
Edward Rezaimehr <erezaimehr@SREZAIMEHR-MBP.local>
2015-01-31 19:26:43 -0800

    printing data from post live feed

--- routes/insta.js
+++ routes/insta.js
@@ -8,7 +8,15 @@ router.get('/post', function(req, res, next) {
 });
 
 router.post('/post', function(req, res, next) {
-	console.log(req.body.data);
+   // [ { changed_aspect: 'media',
+   //     object: 'user',
+   //     object_id: '1686295026',
+   //     time: 1422755996,
+   //     subscription_id: 16642046,
+   //     data: [Object] } ],
+	req.body.forEach(function(currentValue){
+		console.log(currentValue);
+	});
 	res.send('respond with a resource');
 });
 
