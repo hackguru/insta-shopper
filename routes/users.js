53c10f9436a310fdbc5f2717932ce009027892ac
Sina Rezaimehr <edwardmehr@Sinas-Air.home>
2015-03-10 13:32:16 -0700

    opning posts api for query by all

--- routes/users.js
+++ routes/users.js
@@ -149,10 +149,6 @@ router.get('/:userId/likedMedias', function(req, res, next) {
 });
 
 router.get('/:userId/postedMedias', function(req, res, next) {
-	if(!req.user || req.user._id != req.params.userId){
-		res.status(401).json({ error: 'unauthorized user' });
-		return;
-	}
 	var count = req.query.count || 0;
 	var startDate = (req.query.startDate) ? Date.parse(req.query.startDate) : undefined;
 	var endDate = (req.query.endDate) ? Date.parse(req.query.endDate) : undefined;
